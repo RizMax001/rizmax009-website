55ac313681e98b82d2c786c796e04e7bd8587503
RizMax001 <awsstok@gmail.com>
2025-06-13 20:10:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  openaiApiKey: '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', // Ganti dengan API key kamu
+  openaiApiKey: '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', // Ganti dengan API key kamu
   creator: 'Rizky Max',
 };
 
