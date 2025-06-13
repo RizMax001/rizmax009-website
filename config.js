26ef0f383211b76385beeb6cd6b902939975709d
RizMax001 <awsstok@gmail.com>
2025-06-13 19:52:36 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+  openaiApiKey: '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', // Ganti dengan API key kamu
+  creator: 'Rizky Max',
+};
+
+export default config;
