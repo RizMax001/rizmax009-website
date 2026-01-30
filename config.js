529537dab6bdc5acc14f21e2a001a1a36b6eeefd
RizMax001 <awsstok@gmail.com>
2026-01-30 14:48:46 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const config = {
-  openaiApiKey: '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', // Ganti dengan API key kamu
-  creator: 'Rizky Max',
-};
-
-export default config;
